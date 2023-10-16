2b3365c959facec8cca7dbed4655642481e579a5
Annapurna555 <pawelkarabowicz@gmail.com>
2023-10-16 21:45:30 +0200

    Initial Commit

---
+++ Laboratorium3/Laboratorium3App.swift
@@ -0,0 +1,17 @@
+//
+//  Laboratorium3App.swift
+//  Laboratorium3
+//
+//  Created by Paweł Karabowicz on 16/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Laboratorium3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Laboratorium3UITests/Laboratorium3UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Laboratorium3UITestsLaunchTests.swift
+//  Laboratorium3UITests
+//
+//  Created by Paweł Karabowicz on 16/10/2023.
+//
+
+import XCTest
+
+final class Laboratorium3UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
